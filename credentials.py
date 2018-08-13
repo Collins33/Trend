06eba815a385eeb47ae92a53ab712741769abecf
Collins Njau <collinsnjau39@gmail.com>
2018-08-13 10:25:00 +0300

    Fix the tweepy import error

--- credentials.py
+++ credentials.py
@@ -1,3 +1,24 @@
 import os
+import tweepy
+from time import sleep
 
-print(os.environ['CONSUMER_KEY'])
+# get the credentials
+environment_key = os.environ
+consumer_key = environment_key['CONSUMER_KEY']
+consumer_secret = environment_key['CONSUMER_SECRET']
+access_token = environment_key['ACCESS_TOKEN']
+access_token_secret = environment_key['ACCESS_TOKEN_SECRET']
+
+# authenticate our bot
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# initialize the api
+api = tweepy.API(auth)
+# # wait on limit being set to true means the
+# code will not crash when it reaches rate
+# limit
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print (tweet.text)
